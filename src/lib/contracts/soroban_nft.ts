c84ed57d4fb029c0a71ae48c2101668fe7486e55
Armando Medina <113925592+armsves@users.noreply.github.com>
2025-06-22 09:56:02 +0200

    read and write contract calls working

--- src/lib/contracts/soroban_nft.ts
+++ src/lib/contracts/soroban_nft.ts
@@ -4,4 +4,7 @@ import * as Client from "soroban_nft"; // import the package we just added as a 
 export default new Client.Client({
   ...Client.networks.testnet, // this includes the contract address and network passphrase
   rpcUrl: "https://soroban-testnet.stellar.org", // this is required to invoke the contract through RPC calls
+  allowHttp: true,
+  publicKey: undefined, // this is optional, but if you want to use the client to sign transactions, you need to set it
 });
+
